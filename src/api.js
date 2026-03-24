43f88bd0e972f6009403e62a1bfb49630bcaaf23
Mr-Thanush <thanush.2645@gmail.com>
2026-03-24 17:59:03 +0530

    i have changed the link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
  import axios from "axios";
 
  const api=axios.create({
-    baseURL:"http://localhost:8080/api/authentication",
+    baseURL:"https://authentication-k94y.onrender.com/api/authentication",
     withCredentials:true  //it is must for cookies
  });
 
